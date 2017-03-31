f73c98af0736beaa1d62eae5d133e56c47723392
Eugenio <eugenionlopez@gmail.com>
2017-03-31 20:47:00 -0300

    Fixes startup redis cache

--- config/initializers/set_startup_redis_cache.rb
+++ config/initializers/set_startup_redis_cache.rb
@@ -3,7 +3,9 @@
   #Stores all unique codigo_externo to redis on app start - Unless env is test since that stops seeds from working.
   
 
-  Result.set_all_unique_codigo_externo_to_redis unless Rails.env == "test"
+  if Result
+    Result.set_all_unique_codigo_externo_to_redis unless Rails.env == "test"
+  end
 
   #Stores all organismos publicos data. get => Redis.current.hgetall("organismos_publics")
   # Stores all estados_licitacion data. get => Redis.current.hgetall("estados_licitacion")
