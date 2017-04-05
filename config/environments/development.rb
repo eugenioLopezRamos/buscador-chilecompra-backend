295d0f11fdd9a1f41a7aca8b2b004084fc78ae80
Eugenio <eugenionlopez@gmail.com>
2017-04-05 14:30:06 -0300

    Adds i18n fallbacks

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.i18n.fallbacks = [:en]
   # Show full error reports.
   config.consider_all_requests_local = true
 
