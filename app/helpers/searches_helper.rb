51dd72e56ad9dae4ada7f17174d36a1caee2cd5d
Eugenio <eugenionlopez@gmail.com>
2017-04-07 19:02:36 -0300

    Fixes hashify

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -2,7 +2,7 @@
 module SearchesHelper
   def show_searches(user)
     searches = user.searches.pluck(*show_fields)
-    hashify(show_fields.flatten, 0, searches.flatten, {}).delete_if { |_key, value| value == [nil] }
+    hashify.call(show_fields.flatten, 0, searches.flatten, {}).delete_if { |_key, value| value == [nil] }
   end
 
   def populate_response(response, search_name)
@@ -18,22 +18,23 @@ module SearchesHelper
     %w(name value id)
   end
 
-  def hashify(*show_fields, start_index, plucked_array, new_hash)
-    # TODO: Document this in detail. TL;DR
-    # => makes a hash like
-    # {"name": [name1, name2, name3...],
-    #  "value": [{value1...}, {value2...}],
-    #  "id":[id1, id2, id3...]
-    #  }
-    fields_length = show_fields.length
-    batch = plucked_array.slice(start_index, fields_length)
+  def hashify
+    # TODO: Document this in detail. TL;DR => makes a hash like {"name": [name1, name2, name3...], "value": [{value1...}, {value2...}], "id":[id1, id2, id3...]}
+    lambda do |*show_fields, start_index, plucked_array, new_hash|
+      len = show_fields.flatten.length
+      batch = plucked_array.flatten.slice(start_index, len)
 
-    show_fields.each_with_index do |key, i|
-      batch_value = batch[i]
-      new_hash[key] = new_hash[key] ? new_hash[key].push(batch_value) : [].push(batch_value)
+      show_fields.flatten.each_with_index do |key, i|
+        new_hash[key] = if !new_hash[key]
+                          [].push(batch[i])
+                        else
+                          new_hash[key].push(batch[i])
+                        end
+      end
+      new_index = start_index + len
+
+      return new_hash if new_index >= plucked_array.flatten.length
+      hashify.call(show_fields, new_index, plucked_array, new_hash)
     end
-    new_index = start_index + fields_length
-    return new_hash if new_index >= plucked_array.length
-    hashify(show_fields, new_index, plucked_array, new_hash)
   end
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 ## Change these
-server "#{ENV['CHILECOMPRA_RAILS_SERVER_IP']}", user: "#{ENV['CHILECOMPRA_RAILS_DEPLOY_USER']}", port: 22, roles: [:web, :app, :db], primary: true
+server (ENV['CHILECOMPRA_RAILS_SERVER_IP']).to_s, user: (ENV['CHILECOMPRA_RAILS_DEPLOY_USER']).to_s, port: 22, roles: [:web, :app, :db], primary: true
 
-set :repo_url,        "#{ENV['CHILECOMPRA_RAILS_REPO']}"
-set :application,     "#{ENV['CHILECOMPRA_RAILS_APP_NAME']}"
-set :user,            "#{ENV['CHILECOMPRA_RAILS_DEPLOY_USER']}"
+set :repo_url,        (ENV['CHILECOMPRA_RAILS_REPO']).to_s
+set :application,     (ENV['CHILECOMPRA_RAILS_APP_NAME']).to_s
+set :user,            (ENV['CHILECOMPRA_RAILS_DEPLOY_USER']).to_s
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
@@ -18,10 +18,10 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %W(#{ENV['CHILECOMPRA_RAILS_KEY_LOCATION']}) }
+set :ssh_options,     forward_agent: true, user: fetch(:user), keys: %W(#{ENV['CHILECOMPRA_RAILS_KEY_LOCATION']})
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, false  # Change to false when not using ActiveRecord
+set :puma_init_active_record, false # Change to false when not using ActiveRecord
 
 set :migration_role, :app
 ## Defaults:
@@ -32,7 +32,7 @@ set :migration_role, :app
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-#set :linked_files, %w{config/database.yml}
+# set :linked_files, %w{config/database.yml}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
@@ -48,12 +48,12 @@ namespace :puma do
 end
 
 namespace :deploy do
-  desc "Make sure local git is in sync with remote."
+  desc 'Make sure local git is in sync with remote.'
   task :check_revision do
     on roles(:app) do
       unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
+        puts 'WARNING: HEAD is not the same as origin/master'
+        puts 'Run `git push` to sync changes.'
         exit
       end
     end
@@ -75,11 +75,11 @@ namespace :deploy do
   end
 
   before :starting,     :check_revision
- # after  :finishing,    :compile_assets
+  # after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
 
-#ps aux | grep puma    # Get puma pid
+# ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
 # kill -s SIGTERM pid   # Stop puma
